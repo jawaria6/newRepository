6ecb4a5d36e566d93dd6dea7fce65376d680d48c
jawaria6 <mcsf19m006@pucit.edu.pk>
2021-05-27 17:47:34 +0500

    fragments

---
+++ Fragments/app/src/main/java/com/example/fragments/Fragment2.java
@@ -0,0 +1,43 @@
+package com.example.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+
+public class Fragment2 extends Fragment {
+
+
+
+    public Fragment2() {
+        // Required empty public constructor
+    }
+
+
+
+    Button button;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_2, container, false);
+
+        button = view.findViewById(R.id.btn_frgm2);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getContext(), "Fragment Two Button", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        return view;
+    }
+}
